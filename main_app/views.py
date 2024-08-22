52fd2750c1faed0591a4efc005aa77bc3e162de9
BaileyBeck <baileybk2@gmail.com>
2024-08-22 15:57:34 -0400

    rendered olane data in index page

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+class Plane:
+    def __init__(self, name, model, category, image):
+        self.name = name
+        self.model = model 
+        self.category = category
+        self.image = image
+
+planes = [
+    Plane('Seminole', 'PA-44', 'GA', 'image'),
+    Plane('King Air', 'King Air 360ER', 'P', 'image'),
+    Plane('Piper Warrior III', 'PA-28-151', 'GA', 'image'),
+]
 
 def home(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def plane_index(request):
+    return render(request, 'planes/index.html', {'planes': planes})
