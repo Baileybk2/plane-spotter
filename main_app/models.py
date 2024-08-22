aa4e6bcea86f056e523030b2db5243de7a34273b
BaileyBeck <baileybk2@gmail.com>
2024-08-22 16:20:07 -0400

    created the plane model and planes psql database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Plane(models.Model):
+    name = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    category = models.CharField(max_length=2)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
