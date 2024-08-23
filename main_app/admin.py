80300ecd5ad556c6d1b783cb38dff95faf4164eb
BaileyBeck <baileybk2@gmail.com>
2024-08-23 14:55:12 -0400

    added Sighting model and admin testing successful

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Plane
+from .models import Plane, Sighting
 
 admin.site.register(Plane)
+admin.site.register(Sighting)
--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 
 CATEGORY = (
     ('GA', 'General Aviation'),
@@ -17,9 +18,21 @@ class Plane(models.Model):
         max_length=2, 
         choices=CATEGORY, 
         default=CATEGORY[0][0])
+    image = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('plane-detail', kwargs={'plane_id': self.id})
+    
+class Sighting(models.Model):
+    datetime = models.DateTimeField(default=timezone.now)
+    location = models.TextField(max_length=250)
+    registration = models.URLField(blank=True, null=True)
+    tracking = models.URLField(blank=True, null=True)
+
+    plane = models.ForeignKey(Plane, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Spotted on {self.datetime} at {self.location}"
